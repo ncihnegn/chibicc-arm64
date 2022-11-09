10b21cbaf09ef4242c1682bdecca7fc9280188a2
ncihnegn <ncihnegn@users.noreply.github.com>
2022-11-09 00:49:03 -0800

    Add + and - operators

--- main.c
+++ main.c
@@ -1,16 +1,132 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+// Token kind
+typedef enum {
+  TK_RESERVED, // Keywords or punctuators
+  TK_NUM,      // Integer literals
+  TK_EOF,      // End-of-file markers
+} TokenKind;
+
+// Token type
+typedef struct Token Token;
+struct Token {
+  TokenKind kind; // Token kind
+  Token *next;    // Next token
+  int val;        // If kind is TK_NUM, its value
+  char *str;      // Token string
+};
+
+// Current token
+Token *token;
+
+// Report an error and exit
+void error(char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+// Consumes the current token if it matches `op`.
+bool consume(char op) {
+  if (token->kind != TK_RESERVED || token->str[0] != op)
+    return false;
+  token = token->next;
+  return true;
+}
+
+// Ensure that the current token if it matches `op`.
+void expect(char op) {
+  if (token->kind != TK_RESERVED || token->str[0] != op)
+    error("expected '%c'", op);
+  token = token->next;
+}
+
+// Ensure the current token is TK_NUM.
+int expect_number() {
+  if (token->kind != TK_NUM)
+    error("expected a number");
+  int val = token->val;
+  token = token->next;
+  return val;
+}
+
+bool at_eof() { return token->kind == TK_EOF; }
+
+// Create a new token and add it as the next token of `cur`.
+Token *new_token(TokenKind kind, Token *cur, char *str) {
+  Token *tok = calloc(1, sizeof(Token));
+  tok->kind = kind;
+  tok->str = str;
+  cur->next = tok;
+  return tok;
+}
+
+// Tokenize `p` and returns new tokens.
+Token *tokenize(char *p) {
+  Token head;
+  head.next = NULL;
+  Token *cur = &head;
+
+  while (*p) {
+    // Skip whitespace characters.
+    if (isspace(*p)) {
+      p++;
+      continue;
+    }
+
+    // Punctuator
+    if (*p == '+' || *p == '-') {
+      cur = new_token(TK_RESERVED, cur, p++);
+      continue;
+    }
+
+    // Integer literal
+    if (isdigit(*p)) {
+      cur = new_token(TK_NUM, cur, p);
+      cur->val = strtol(p, &p, 10);
+      continue;
+    }
+
+    error("invalid token");
+  }
+
+  new_token(TK_EOF, cur, p);
+  return head.next;
+}
 
 int main(int argc, char **argv) {
   if (argc != 2) {
-    fprintf(stderr, "%s: invalid number of arguments\n", argv[0]);
+    error("%s: invalid number of arguments", argv[0]);
     return 1;
   }
 
+  // Assembly header
   printf("\t.global _main\n");
   printf("\t.p2align 2\n");
   printf("_main:\n");
-  printf("\tmov\tw0, #%d\n", atoi(argv[1]));
+
+  token = tokenize(argv[1]);
+
+  // The first token must be a number
+  printf("\tmov\tw0, #%d\n", expect_number());
+
+  // ... followed by either `+ <number>` or `- <number>`.
+  while (!at_eof()) {
+    if (consume('+')) {
+      printf("\tadd\tw0, w0, #%d\n", expect_number());
+      continue;
+    }
+
+    expect('-');
+    printf("\tsub\tw0, w0, #%d\n", expect_number());
+  }
   printf("\tret\n");
   return 0;
 }
